b4918edb7e44bcc3e4916c6be6dc3458b78f6546
JacekWarzecha <drjackys@gmail.com>
2023-04-17 22:35:31 +0200

    added contact component

---
+++ src/features/aboutme/Contact/styled.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Address = styled.address`
+  display: grid;
+  grid-template-rows: auto auto auto auto;
+  font-style: normal;
+`;
+
+export const Paragraph = styled.p`
+  font-size: 12px;
+  letter-spacing: normal;
+  font-weight: 700;
+  line-height: 130%;
+  margin: 0;
+  padding-bottom: 24px;
+`;
+
+export const Mail = styled.a`
+  font-weight: 900;
+  font-size: 32px;
+  line-height: 39px;
+  color: ${({ theme }) => theme.color.mineShaft};
+  text-decoration: none;
+  padding-bottom: 24px;
+`;
+
+export const Description = styled.p`
+  color: ${({ theme }) => theme.color.mineShaft};
+  max-width: 670px;
+  font-size: 18px;
+  margin: 0;
+  padding-bottom: 56px;
+`;
+
+export const Icons = styled.dl`
+  display: flex;
+  margin: 0;
+`;
+
+export const IconLink = styled.a`
+  margin-right: 24px;
+  margin-bottom: 109px;
+`;
---
+++ src/features/aboutme/mail.js
@@ -0,0 +1,1 @@
+export const mail = "drjackys@gmail.com";
