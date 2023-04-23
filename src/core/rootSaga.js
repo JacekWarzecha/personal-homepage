d92bd0ac65f46df371634fe059807ff307f6c0a8
JacekWarzecha <drjackys@gmail.com>
2023-04-23 21:40:39 +0200

    'core' created, rootSaga, store move into them, added themeReducer to store

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,5 +1,5 @@
 import { all } from "redux-saga/effects";
-import { repositoriesSaga } from "./features/aboutme/repositoriesSaga";
+import { repositoriesSaga } from "../features/aboutme/repositoriesSaga";
 
 export default function* rootSaga() {
   yield all([repositoriesSaga()]);
--- src/core/store.js
+++ src/core/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import repositoriesReducer from "./features/aboutme/repositoriesSlice";
+import repositoriesReducer from "../features/aboutme/repositoriesSlice";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./rootSaga";
+import themeReducer from "../common/themeSlice";
 
 const sagaMiddleWare = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     repositories: repositoriesReducer,
+    theme: themeReducer,
   },
   middleware: [sagaMiddleWare],
 });
