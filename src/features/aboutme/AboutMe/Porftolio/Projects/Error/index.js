177cf955fc3004b571d31e7e7868587e6355e5fe
JacekWarzecha <drjackys@gmail.com>
2023-04-29 22:18:52 +0200

    Error Component updated

--- src/features/aboutme/AboutMe/Porftolio/Projects/Error/index.js
+++ src/features/aboutme/AboutMe/Porftolio/Projects/Error/index.js
@@ -1,13 +1,29 @@
-import { Wrapper, H3, Paragraph } from "./styled";
+import {
+  Wrapper,
+  H3,
+  Paragraph,
+  StyledDangerIcon,
+  StyledButtonLink,
+} from "./styled";
+import { socials } from "../../../Contact/SocialIcons/social";
 
 export const Error = () => {
   return (
     <Wrapper>
+      <StyledDangerIcon />
       <H3>Ooops! Something went wrong...</H3>
       <Paragraph>
         Sorry, failed to load Github projects. <br />
         You can check them directly on Github.
       </Paragraph>
+      <StyledButtonLink
+        href={socials[0].url}
+        target="_blank"
+        title={socials[0].url}
+        rel="noreferrer"
+      >
+        Go to Github
+      </StyledButtonLink>
     </Wrapper>
   );
 };
--- src/features/aboutme/AboutMe/Porftolio/Projects/Error/styled.js
+++ src/features/aboutme/AboutMe/Porftolio/Projects/Error/styled.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
+import { ReactComponent as DangerIcon } from "./Danger.svg";
+import { ButtonLink } from "../../../ButtonLink";
 
 export const Wrapper = styled.div`
   color: ${({ theme }) => theme.colors.textPrimary};
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+export const StyledDangerIcon = styled(DangerIcon)`
+  margin-top: 95px;
+  margin-bottom: 23px;
 `;
 
 export const H3 = styled.h3`
@@ -17,5 +24,10 @@ export const H3 = styled.h3`
 
 export const Paragraph = styled.p`
   text-align: center;
-  margin: 0;
+  margin: 0 0 32px;
+`;
+
+export const StyledButtonLink = styled(ButtonLink)`
+  margin-bottom: 25px;
+  line-height: 24px;
 `;
